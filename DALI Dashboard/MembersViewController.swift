b075b073e3cb23947a42178fbc6932fcee7674fa
Shreyas Agnihotri <shreyas.v.agnihotri@gmail.com>
2019-02-02 19:11:19 -0500

    Tested tableView display with sample array of names

--- DALI Dashboard/MembersViewController.swift
+++ DALI Dashboard/MembersViewController.swift
@@ -10,13 +10,27 @@ import UIKit
 
 class MembersViewController: UITableViewController {
 
-    
+    let memberList = ["John", "Juan", "Tim"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
 
+    //MARK - TableView data source methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memberList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "member", for: indexPath)
+        cell.textLabel?.text = memberList[indexPath.row]
+        
+        return cell
+    }
+    
     
 }
 
